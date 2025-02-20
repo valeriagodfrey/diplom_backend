164bfad8e4e78a1ca9af63d991feaeb035a85feb
valeriagodfrey <ewagodfrey@gmail.com>
2025-02-20 03:45:50 +0200

    ddda

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
   app.enableCors({
-    origin: 'http://localhost:5173', // Замените на ваш фронтенд домен
+    origin: 'https://diplom-front-h4do.onrender.com', // Замените на ваш фронтенд домен
     credentials: true, // Разрешить отправку куки
   });
 
